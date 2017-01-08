8a720e5a0a1aff8480d972aa126b0156a500ef4b
SekiguchiKai <sekiguchikai@SekiguchiKai-1.local>
2017-01-08 13:17:34 +0900

    配列を使った簡単なngForの例を作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+import { Language } from './lamguage';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>{{title}}</h1>
+  <p>Languages: </p>
+  <ul>
+    <li *ngFor="let lang of langs">
+    {{lang.name}}
+    </li>
+  </ul>
+  
+  `
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Programming Language!';
+
+  langs = [
+    new Language('JavaScript', 'Dynamic'),
+    new Language('TypeScript', 'Static'),
+    new Language('Java', 'Static'),
+    new Language('Python', 'Dynamic'),
+    new Language('Go', 'Static'),
+    new Language('Ruby', 'Dynamic')
+  ];
+
+  myLamg = this.langs[0];
+
+
+
 }
---
+++ src/app/lamguage.ts
@@ -0,0 +1,7 @@
+export class Language {
+
+    constructor(
+        public name: string,
+        public type: string,
+    ) { }
+}
